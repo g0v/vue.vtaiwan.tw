47255968bcd628fecbc21130cfed738cf33be9c4
Ly Cheng <lyforever62@hotmail.com>
2016-10-08 16:15:12 +0800

    Add simple menu and outside border

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
 <template>
   <div id="app">
-    <div class="header">
-      <div class="inner">
-        <router-link to="/" exact>
-          <img class="logo" src="./assets/logo.png" alt="logo">
-        </router-link>
-      </div>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/" exact>
+            <img class="logo" src="./assets/logo.png" alt="logo">
+            vTaiwan
+          </router-link>
+        </li>
+        <li>Comment 留言不忘返</li>
+        <li>Live Draft 直播寫草案</li>
+        <li>Follow PROPOSAL 來追蹤定案</li>
+      </ul>
+    </nav>
     <transition name="fade" mode="out-in">
       <router-view class="view"></router-view>
     </transition>
   </div>
 </template>
+
+<style lang="stylus">
+*
+  box-sizing border-box
+body
+  border 4px solid #D75155
+  height 100vh
+  margin 0
+nav
+  a
+    text-decoration none
+    &:visited
+      color black
+  .logo
+    width 40px
+    height 40px
+  ul
+    width 100%
+    text-align center
+    padding-left 0
+    list-style none
+  li
+    display inline-block
+    float none
+</style>
