25657653d9555a1ab809d87a1bd85c194e2c132c
Ly Cheng <lyforever62@hotmail.com>
2016-11-12 15:21:33 +0800

    Add passport-discourse support related #7

--- server.js
+++ server.js
@@ -11,9 +11,38 @@ const passportDiscourse = require('passport-discourse').Strategy
 
 const app = express()
 
-
 app.use('/dist', express.static(resolve('./dist')))
 app.use(favicon(resolve('./src/assets/logo.png')))
+app.use(require('express-session')({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new passportDiscourse({
+  secret: process.env.SECRET,
+  discourse_url: 'https://talk.vtaiwan.tw',
+  debug: false,
+}, function(accessToken, refreshToken, profile, done) {
+  done(null, profile)
+}))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
+app.get('/auth/discourse_sso', passport.authenticate('discourse'))
+app.get(passportDiscourse.route_callback, passport.authenticate('discourse', {
+  successRedirect: '/',
+  failureRedirect: '/',
+}))
+
 
 app.get('*', render)
 
