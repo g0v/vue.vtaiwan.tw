2894f49105d1acafd398e3eb0af9c6ed0da5bc61
Bestian Tang 唐宗浩 <besitan@gmail.com>
2016-11-14 12:25:58 +0800

    navbar add search photo

--- src/components/app_navbar.vue
+++ src/components/app_navbar.vue
@@ -2,67 +2,99 @@
 
 @import "../sass/global.scss";
 
+$navHeight: 55px;
+
 .nav {
   display: flex;
+  height: $navHeight;
+  overflow: visible;
   align-items: center;
   background: rgba(255, 255, 255, 0.8);
 }
 
-.logo {
+.logo, .explore {
   display: flex;
   align-items: center;
   justify-content: space-around;
+}
+
+.logo {
   width: 90px;
   margin-left: 30px;
   margin-right: 20px;
 }
 
-.main {
+.explore {
+  height: $navHeight;
+  min-width: 4em;
+  &.router-link-active {
+    background-color: white;
+  }
+}
+
+form.search {
+  padding: 5px;
+  i.icon {
+    position: relative;
+    z-index: -1;
+    left: 1.6em;
+    color: white;
+    font-size: 1.4em;
+  }
+  input {
+    height: 2.5em;
+    border: 1px solid white;
+    border-radius: 1em;
+    background-color: transparent;
+  }
+  &.active {
+    i.icon {
+      visibility: hidden;
+    }
+  }
+}
+
+.null {
   flex: 1;
 }
 
 .tab {
-  ul {
-    width: 100%;
-    text-align: center;
-    padding-left: 0;
-    list-style: none;
-    font-size:0;
-    li {
-      display: inline-block;
-      float: none;
-      font-size: 1rem;
-      a {
-        color: #333;
-        background-color: white;
-        cursor: pointer;
-        display: inline-block;
-        font-size: 0.6rem;
-        text-decoration: none;
-        &:visited {
-          color: #030;
-        }
-        &:hover {
-          border-bottom: 3px solid green;
-        }
-        &.active, &.router-link-active {
-          background-color: $main;
-          color: white;
-          &.join { background-color: $join }
-          &.comment { background-color: $comment }
-          &.live { background-color: $live }
-          &.track { background-color: $track }
-        }
-        @include transition(background-color 0.5s ease);
-        padding: 10px;
+  display: flex;
+  margin-left: auto !important;
+  a.item {
+    display: flex;
+    align-items: center;
+    -webkit-box-align: center;
+    vertical-align: middle;
+    justify-content: center;
+    color: #333;
+    height: $navHeight;
+    cursor: pointer;
+    font-size: 1rem;
+    text-decoration: none;
 
-        border-left: 1px solid #ccc;
+    &:visited {
+      color: #030;
+    }
+    &:hover {
+  //    border-bottom: 3px solid green;
+    }
+    &.active, &.router-link-active {
+      background-color: $main;
+      color: white;
+      &.join { background-color: $join }
+      &.comment { background-color: $comment }
+      &.live { background-color: $live }
+      &.track { background-color: $track }
+    }
+    @include transition(background-color 0.5s ease);
+    padding: 10px;
 
-        .sub {
-          font-size: 0.7rem;
-          line-height: 1rem;
-        }
-      }
+    border-left: 1px solid #ccc;
+
+    .sub {
+      font-size: 0.7rem;
+      line-height: 1rem;
     }
   }
 }
@@ -70,16 +102,21 @@
 
 <template lang="jade">
 nav.nav
-  router-link(to='/', exact='').logo
+  router-link.logo(to='/', exact='')
     img(width='30', height='30', src='../assets/logo.png', alt='logo')
     span vTaiwan
-  .main
-  .tab
-    ul
-      li(v-for='r in routes', v-if="r.r")
-        router-link(:to="'/'+r.r", :class='r.r', exact='')
-          .sub {{r.en | uppercase}}
-          .main {{r.t}}
+  router-link.explore(to='/', exact='')
+    | 探索
+  form.search(:class="{active: key}")
+    i.search.icon
+    input(type="search", v-model="key")
+    span {{key}}
+  .null
+  .tab    
+    router-link.item(v-for='r in routes', v-if="r.r", :to="'/'+r.r", :class='r.r', exact='')
+      {{ r.t }}
+      //.sub {{r.en | uppercase}}
+        
 </template>
 
 <script>
@@ -88,6 +125,7 @@ export default {
   props: ['routes'],
   data () {
     return {
+      key: '',
       data: '...'
     }
   }
