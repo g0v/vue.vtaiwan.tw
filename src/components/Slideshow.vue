2dbfc1e8042555bb1623cd81aaadc6b2b0892fe1
Bestian Tang <besitan@gmail.com>
2016-11-14 14:02:30 +0800

    add two arrows

--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -1,14 +1,49 @@
 <style lang="scss" scoped>
   .component {
+    position: relative;
+    top: 54px;
+    margin-bottom: 54px;
     width: 100%;
     img {
       width: 100%;
+      z-index: 3;
     }
   }
+
+  a {
+    cursor: pointer;
+    &:hover {
+      i {
+        color: white !important;
+      }
+    }
+    &.pre {
+      position: absolute;
+      z-index: 5;
+      top: 33vh;
+      left: 5px;
+    }
+    &.next {
+      position: absolute;
+      z-index: 5;
+      top: 33vh;
+      right: 5px;
+    }
+  }
+
+  i {
+    transition: color 0.3s;
+  }
+
+  
 </style>
 
 <template lang="jade">
   .component
+    a.pre
+      i.huge.chevron.black.left.icon
+    a.next
+      i.huge.chevron.black.right.icon
     img(src="http://static.thousandwonders.net/Taiwan.original.3738.jpg")
 
 
