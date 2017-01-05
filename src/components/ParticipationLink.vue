010253b6acb5fb2c2ae575d27622e99ee34e4322
smith <swith02620@ey.gov.tw>
2017-01-05 11:30:31 +0800

    icon 變更

--- src/components/ParticipationLink.vue
+++ src/components/ParticipationLink.vue
@@ -1,9 +1,7 @@
 <template lang="jade">  
- 
-  .urllink(v-if = "ulinkall.length > 0")
-   span.urllink(v-for="(item, index) in ulinkall")
-    span(v-html = "ulinkall[index]")
-    
+ .urllink(v-if = "ulinkall.length > 0")
+  span.urllink(v-for="(item, index) in ulinkall")
+   a(v-html = "ulinkall[index]")
 </template>
 
 <script>
@@ -14,49 +12,83 @@ export default {
   data () {
     return {
       ulinkall:[],
+      
       data_base:[
         {
           key: 'hackpad',
-          icon:'https://upload.wikimedia.org/wikipedia/commons/b/b0/Writing_Circle.svg'
+          icon:"<i class='edit icon'></i>共筆"
         },
         {
           key: 'sayit',
-          icon:'https://talk.pdis.nat.gov.tw/uploads/default/original/1X/0702e94b7c83e63a7c33826c4552e1d2a540d860.png'
+          icon:"<i class='book icon'></i>記錄"
         },
         {
           key: 'youtube',
-          icon:'https://upload.wikimedia.org/wikipedia/commons/3/37/Youtube.svg',
+          icon:"<i class='play icon'></i>影片",
         },
         {
           key: 'pol.is',
-          icon:'https://talk.pdis.nat.gov.tw/uploads/default/original/1X/c56e47b659f9f1298de3f43abb3777b5f21aad2f.PNG',
+          icon:"<i class='users icon'></i>討論"
         },
         {
           key: 'talk.vtaiwan.tw',
-          icon:'http://www.discourse.org/images/logo.png',
+          icon:"<i class='comment outline icon'></i>留言"
         },
         {
           key: 'app.sli.do',
-          icon:'https://talk.pdis.nat.gov.tw/uploads/default/original/1X/3d16d62bc946033dfefb4029a5149d3f3420aebc.png',
+          icon:"<i class='bullhorn icon'></i>提問"
         },
+        {
+          key: 'PDF',
+          icon:"<i class='text file outline icon'></i>PDF"
+        },
+        
       ]
     }
   },
   created:function(a,b){
-    console.log(this.urllink.length)
     for(var i=0;i<this.urllink.length;i++){
       for(var j=0; j<this.data_base.length ;j++){
         if(this.urllink[i].indexOf(this.data_base[j].key)!=-1){
-          this.ulinkall.push("<a href= "+this.urllink[i]+" target='_blank'><img src="+this.data_base[j].icon+" width='50'' height='50'></a>");
+          this.ulinkall.push("<a href= "+this.urllink[i]+" target='_blank'>"+this.data_base[j].icon+"</a>");
         }
       }            
     }
-    
   }
 }
 
 </script>
 <style lang="scss" scoped>
+  a {
+      font-size: 2em;
+      margin-left: 20px;
+  }
 </style>
 
 
+// data_base:[
+//         {
+//           key: 'hackpad',
+//           icon:'https://upload.wikimedia.org/wikipedia/commons/b/b0/Writing_Circle.svg'
+//         },
+//         {
+//           key: 'sayit',
+//           icon:'https://talk.pdis.nat.gov.tw/uploads/default/original/1X/0702e94b7c83e63a7c33826c4552e1d2a540d860.png'
+//         },
+//         {
+//           key: 'youtube',
+//           icon:'https://upload.wikimedia.org/wikipedia/commons/3/37/Youtube.svg',
+//         },
+//         {
+//           key: 'pol.is',
+//           icon:'https://talk.pdis.nat.gov.tw/uploads/default/original/1X/c56e47b659f9f1298de3f43abb3777b5f21aad2f.PNG',
+//         },
+//         {
+//           key: 'talk.vtaiwan.tw',
+//           icon:'http://www.discourse.org/images/logo.png',
+//         },
+//         {
+//           key: 'app.sli.do',
+//           icon:'https://talk.pdis.nat.gov.tw/uploads/default/original/1X/3d16d62bc946033dfefb4029a5149d3f3420aebc.png',
+//         },
+//       ]
