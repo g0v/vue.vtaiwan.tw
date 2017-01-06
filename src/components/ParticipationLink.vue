cc26357506f0422680b770579c8ada2c6e323955
smith <swith02620@ey.gov.tw>
2017-01-06 09:59:02 +0800

    更改按鍵

--- src/components/ParticipationLink.vue
+++ src/components/ParticipationLink.vue
@@ -1,7 +1,9 @@
 <template lang="jade">  
+ //<div class='column'><i class='con edit icon'>Archive</i></div>
+ 
  .urllink(v-if = "ulinkall.length > 0")
   span.urllink(v-for="(item, index) in ulinkall")
-   a(v-html = "ulinkall[index]")
+   span(v-html = "ulinkall[index]")
 </template>
 
 <script>
@@ -12,35 +14,35 @@ export default {
   data () {
     return {
       ulinkall:[],
-      
+     gg:"<div class='column'><i class='archive icon'></i>Archive</div>",
       data_base:[
         {
           key: 'hackpad',
-          icon:"<i class='edit icon'></i>共筆"
+          icon:"<i class='con edit icon'> 共筆</i>"
         },
         {
           key: 'sayit',
-          icon:"<i class='book icon'></i>記錄"
+          icon:"<i class='con book icon'> 記錄</i>"
         },
         {
           key: 'youtube',
-          icon:"<i class='play icon'></i>影片",
+          icon:"<i class='con play icon'> 影片</i>",
         },
         {
           key: 'pol.is',
-          icon:"<i class='users icon'></i>討論"
+          icon:"<i class='con users icon'> 討論</i>"
         },
         {
           key: 'talk.vtaiwan.tw',
-          icon:"<i class='comment outline icon'></i>留言"
+          icon:"<i class='con comment outline icon'> 留言</i>"
         },
         {
           key: 'app.sli.do',
-          icon:"<i class='bullhorn icon'></i>提問"
+          icon:"<i class='con bullhorn icon'> 提問</i>"
         },
         {
           key: 'PDF',
-          icon:"<i class='text file outline icon'></i>PDF"
+          icon:"<i class='con text file outline icon'> PDF</i>"
         },
         
       ]
@@ -50,7 +52,7 @@ export default {
     for(var i=0;i<this.urllink.length;i++){
       for(var j=0; j<this.data_base.length ;j++){
         if(this.urllink[i].indexOf(this.data_base[j].key)!=-1){
-          this.ulinkall.push("<a href= "+this.urllink[i]+" target='_blank'>"+this.data_base[j].icon+"</a>");
+          this.ulinkall.push("<div class='ui active button'><a href= "+this.urllink[i]+" target='_blank'>"+this.data_base[j].icon+"</a></div>");
         }
       }            
     }
@@ -58,11 +60,26 @@ export default {
 }
 
 </script>
-<style lang="scss" scoped>
-  a {
-      font-size: 2em;
+<style lang="scss">
+  i.con {
+      font-size: 25px;
       margin-left: 20px;
+      color: #49a3a0;
+      display:inline;
   }
+  .column:hover .icon {
+    color: #009FDA;
+}
+.ui.active.button{
+  background-color: transparent;
+}
+.ui.button {
+    
+    margin: 11x .25em 0 0;
+    padding: 13px 11.5px 8px 0px;
+    border-radius: 81.285714rem;
+}
+
 </style>
 
 
