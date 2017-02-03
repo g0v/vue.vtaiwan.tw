9bea3f85e115a4bd3f6d4d2689961aa524dc6984
john5601 <john560148@gmail.com>
2017-02-03 12:21:41 +0800

    加入註解 移除多餘code

--- src/components/Detail_Topic_Timeline.vue
+++ src/components/Detail_Topic_Timeline.vue
@@ -18,8 +18,8 @@
         .item(v-for = "(ev,idx) in timeline", :class="['dark','gloom','light'][idx % 3]")
           .big 從 {{ev.start}} 開始 {{ev.end}}
           .small 
-            p 進度  
-            .title {{ ev.title }} 
+            p {{ ev.title }}  
+            .title {{ ev.info }} 
           .small 
             p 相關連結
             Plink(:urllink="ev.link")
@@ -46,45 +46,42 @@ export default {
         axios.get('https://talk.vtaiwan.tw/t/'+ this.article.id +'.json?include_raw=1')
         .then((response)=>{
         var detail_info = response.data;
-        var test = detail_info['post_stream']['posts'];
         detail_info = detail_info['post_stream']['posts'].slice(1); // 取得議題時間軸內容
-        if(this.timeline.length === 0){
-            for(var i in detail_info){
-            var regex = /(?: (?:init )?)|\n/g;
-            var date_regex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/g;
-            var time_regex = /^(2[0-3]|1[0-9]|0[0-9]|[^0-9][0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])$/g;
-            var timeline_content = {};
-            var link = {};
-            var links = [];
-            var polis = {};
+
+          for(var i in detail_info){
+            var regex = /(?: (?:init )?)|\n/g; // 用來分開字串
+            var date_regex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/g; //yyyy-mm-dd(ex:2016-10-31)
+            var time_regex = /^(2[0-3]|1[0-9]|0[0-9]|[^0-9][0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])$/g; // hh:mm:ss(ex:19:00:00)
+            var timeline_content = {}; // 時間軸內容
+            var comment = {}; // 暫存處理回覆內容
+            var links = []; // 回覆中的連結
             timeline_content['title'] = detail_info[i]['raw'].split(regex)[0]; // 進度
             timeline_content['start'] = detail_info[i]['raw'].split(regex)[1]; // 開始日期
 
-            link= detail_info[i]['raw'].split(regex); // 每篇回覆
+            comment= detail_info[i]['raw'].split(regex); // 每一個議題簡介(第一篇)底下回覆內容
 
             if(timeline_content['start'].length > detail_info[i]['raw'].split(regex)[2].length){ // 若為"寫草案" 則無結束日期 僅開始日期
                 timeline_content['start'] = timeline_content['start'] + " " + detail_info[i]['raw'].split(regex)[2];
                 timeline_content['end'] = null;
             }
-            else{
+            else{ // 有結束日期
                 timeline_content['end'] = "至 "+ detail_info[i]['raw'].split(regex)[2]; // 結束日期
             }
-            if(detail_info[i]['raw'].split(regex)[2].length > 10){
+            if(detail_info[i]['raw'].split(regex)[2].length > 10){ // 無結束日期
                 timeline_content['end'] = null;
             }
-            for(var j = 1; j < link.length; j++ ){
-                if( link[j].indexOf("http")>-1){
+            for(var j = 1; j < comment.length; j++ ){ // 回覆中的連結處理
+                if( comment[j].indexOf("http")>-1){ // 字串含有"http" -> 連結
                   links.push(detail_info[i]['raw'].split(regex)[j]);
                 }
-                if( link[j].indexOf("http")==-1 && link[j].match(date_regex)==null && link[j].match(time_regex)==null ){
-                  timeline_content['info'] = link[j];
+                if( comment[j].indexOf("http")==-1 && comment[j].match(date_regex)==null && comment[j].match(time_regex)==null ){ // 字串不符合網址、日期、時間等格式 ->  簡介
+                  timeline_content['info'] = comment[j];
                 }
             }
             timeline_content['link'] = links; 
             this.timeline.push(timeline_content);
-         }        
-       }      
-     })
+         }           
+       })
     }
 }
 
