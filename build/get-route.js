a3a90d18198e45bb71f8bd96e282949e22c5156b
mark19891107 <mark19891107@gmail.com>
2017-02-07 16:11:34 +0800

    bug fixed

--- build/get-route.js
+++ build/get-route.js
@@ -1,34 +1,34 @@
 var request = require('request');
 var fs = require("fs");
 var path = 'https://talk.vtaiwan.tw/c/meta-data.json';
-var file_name = './prerender-route.js';
+var file_name = './build/prerender-route.js';
 var prerender = "module.exports = { routes : ['/','/how-to-use','/intro'";
 var headers = {
     'User-Agent': 'Super Agent/0.0.1',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 var opts = {
-	url: path,
-	method: 'GET',
-	headers: headers,
+    url: path,
+    method: 'GET',
+    headers: headers,
 }
-request(opts, function (error, response, results) {
-	if (!error && response.statusCode == 200) {
-		var res = JSON.parse(results);
-		length = res.topic_list.topics.length;
-		//console.log(results);
-		for (var i = 0; i < length; i++) {
-			var title = res.topic_list.topics[i].title;
-			var chk = title.lastIndexOf(' ');
-			var pinned = res.topic_list.topics[i].pinned;
-			if(chk > -1 && pinned==false){
-				title = title.substring(chk+1, title.length);
-				prerender += ",'/"+title+"'";
-			}
-		}
-		prerender += "]}";
-		fs.writeFile(file_name, prerender);
-	}else{
-		console.log('get topic list error='+error+' '+response.statusCode);
-	}
+request(opts, function(error, response, results) {
+    if (!error && response.statusCode == 200) {
+        var res = JSON.parse(results);
+        length = res.topic_list.topics.length;
+        //console.log(results);
+        for (var i = 0; i < length; i++) {
+            var title = res.topic_list.topics[i].title;
+            var chk = title.lastIndexOf(' ');
+            var pinned = res.topic_list.topics[i].pinned;
+            if (chk > -1 && pinned == false) {
+                title = title.substring(chk + 1, title.length);
+                prerender += ",'/topic/" + title + "'";
+            }
+        }
+        prerender += "]}";
+        fs.writeFile(file_name, prerender);
+    } else {
+        console.log('get topic list error=' + error + ' ' + response.statusCode);
+    }
 })
