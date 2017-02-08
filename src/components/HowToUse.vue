148603286e8a94f90b2bfb19763cad90c1f2bc9b
jokeum <joke0111@gmail.com>
2017-02-08 12:16:36 +0800

    update proptab's style

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -1,13 +1,16 @@
 <style scoped lang="scss">
   @import "../sass/global.scss";
+  .component {
+    padding-top: 1em
+  }
 </style>
 
 <template lang="jade">
   .component
     .ui.container
 
-      h2.ui.icon.header
-        i.comment.outline.icon
+      h1.ui.icon.header
+        i.circular.talk.icon
         | 如何有效的發言？
 
       .ui.left.aligned.text.container
@@ -18,8 +21,8 @@
         p 第二，針對討論主題，也歡迎提出詢問。例如，行政機關的文字說明不夠清晰，或者是為了討論需要，希望行政機關能夠提供更多資訊的時候。
         p 第三，若您單純只是認同某個建議，可以直接為它按「讚」，而不用再說一次一樣的東西。若您不認同某個建議，或是某段說明的內容，可以提出建議或詢問，但請避免加入過多的個人情緒，例如直接的批評或是嘲諷，這樣無助於您意見的被採納。
 
-      h2.ui.icon.header
-        i.comments.outline.icon
+      h1.ui.icon.header
+        i.circular.flag.icon
         | 為何有些發言被標示？
 
       .ui.left.aligned.text.container
@@ -27,10 +30,12 @@
         p 用高亮度標示的發言，是版主推薦作為有效發言的範例。這些發言被推薦的原因，不是「內容」寫了什麼，而是具備良好的討論態度及陳述方式。
         p vTaiwan 的版主團隊為中立性質，不會對個別的發言採取支持或反對立場。
 
-      h2.ui.center.aligned.icon.header
-        i.send.outline.icon
+      h1.ui.center.aligned.icon.header
+        i.circular.send.icon
         | 來提個案吧！
-        .sub.header 由下而上的提案，經網路連署後由各部會處理並回應
+
+      .ui.text.container
+        p 由下而上的提案，經網路連署後由各部會處理並回應
 
       // h1.ui.header
       //   i.edit.icon
