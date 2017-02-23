425694dfb8a8edd76dd4f867d97e28a8e8b89387
jokeum <joke0111@gmail.com>
2017-02-23 16:45:57 +0800

    change get-routes to json from js string

--- build/get-route.js
+++ build/get-route.js
@@ -1,8 +1,16 @@
 var request = require('request');
 var fs = require("fs");
 var path = 'https://talk.vtaiwan.tw/c/meta-data.json';
-var file_name = './build/prerender-route.js';
-var prerender = "module.exports = { routes : ['/','/how-to-use','/intro','/search'";
+// var file_name = './build/prerender-route.js';
+let file_name = './build/prerender-route.json';
+// var prerender = "module.exports = { routes : ['/','/how-to-use','/intro','/search'";
+let prerender_json = {} /* output to prerender-route.json */
+let routes = [ /* array for all routes */
+        '/',
+        '/how-to-use',
+        '/intro',
+        'search'
+    ]
 var headers = {
     'User-Agent': 'Super Agent/0.0.1',
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -10,7 +18,7 @@ var headers = {
 var opts = {
     url: path,
     method: 'GET',
-    headers: headers,
+    headers: headers
 }
 request(opts, function(error, response, results) {
     if (!error && response.statusCode == 200) {
@@ -23,11 +31,14 @@ request(opts, function(error, response, results) {
             var pinned = res.topic_list.topics[i].pinned;
             if (chk > -1 && pinned == false) {
                 title = title.substring(chk + 1, title.length);
-                prerender += ",'/topic/" + title + "'";
+                // prerender += ",'/topic/" + title + "'";
+                routes.push("/topic/" + title)
             }
         }
-        prerender += "]}";
-        fs.writeFile(file_name, prerender);
+        // prerender += "]}";
+        prerender_json.routes = routes
+        // fs.writeFile(file_name, prerender);
+        fs.writeFile(file_name, JSON.stringify(prerender_json, null, '\t'));
     } else {
         console.log('get topic list error=' + error + ' ' + response.statusCode);
     }
