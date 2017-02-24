65f9c3848f239e7dd7dff80f42b49bf86f63b91c
john5601 <john560148@gmail.com>
2017-02-24 15:15:47 +0800

    adjust css

--- src/components/app_News.vue
+++ src/components/app_News.vue
@@ -10,11 +10,9 @@
                                 .image
                                     img(:src="n.img_link")
                                 .content
-                                    p {{n.tags}}
+                                    .tags
+                                        .ui.mini.tag.label(v-for="t in n.tags") {{t}}
                                     a.header(:href="n.news_link", target='_blank') {{n.title}}
-                                    //- .meta
-                                    //-     span.right.floated.time 
-                                    //-     //- span.category {{n.source}}
                                 .description 
                                     p.JQellipsis {{n.content}}
                                 .extra.content
@@ -22,7 +20,6 @@
                                         a(:href="'http://'+n.source_link", target='_blank'){{n.source}}
             .swiper-button-prev
             .swiper-button-next                            
-            //- .swiper-pagination
     .thin-only
        .swiper-container2
             .swiper-wrapper
@@ -32,19 +29,17 @@
                                 .image
                                     img(:src="n.img_link")
                                 .content
-                                    p {{n.tags}}
+                                    .tags
+                                        .ui.mini.tag.label(v-for="t in n.tags") {{t}}
                                     a.header(:href="n.news_link", target='_blank') {{n.title}}
-                                    //- .meta
-                                    //-     span.right.floated.time 
-                                    //-     //- span.category {{n.source}}
                                 .description 
                                     p.JQellipsis {{n.content}}
                                 .extra.content
                                     .right.floated.author
                                         a(:href="'http://'+n.source_link", target='_blank'){{n.source}}
-            .swiper-button-prev
-            .swiper-button-next
-            .swiper-pagination 
+            .swiper-pagination
+            //- .swiper-button-prev
+            //- .swiper-button-next
             .swiper-scrollbar 
 
 
@@ -61,25 +56,24 @@ export default {
       observer: true,
       direction: 'horizontal',
       pagination: '.swiper-pagination',
-    //   autoplay: 4000,
+    //   autoplay: 5000,
       slidesPerView: 5,
       paginationClickable: true,
       spaceBetween: 50,
       nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-    //   freeMode: true
+      prevButton: '.swiper-button-prev'
        
     })
     var mySwiper2 =new Swiper ('.swiper-container2', {
       observer: true,
-    //   autoplay: 5000,
+      autoplay: 5000,
       direction: 'horizontal',
       nextButton: '.swiper-button-next',
       prevButton: '.swiper-button-prev',
       pagination: '.swiper-pagination',
+      paginationType: 'fraction',
       scrollbar: '.swiper-scrollbar',
-      spaceBetween: 40,
-    //   keyboardControl: true
+      spaceBetween: 40
     })
     mySwiper1.on('imagesReady', function(){
         var len = 60; // 超過50個字以"..."取代
@@ -103,7 +97,7 @@ export default {
     })
   },
   updated:function(){
-    var len = 50; // 超過50個字以"..."取代
+    var len = 60; // 超過50個字以"..."取代
         $(".JQellipsis").each(function(i){
         if($(this).text().length>len){
             $(this).attr("title",$(this).text());
@@ -126,17 +120,23 @@ export default {
   }
   .ui.card>.content>.header:not(.ui), .ui.cards>.card>.content>.header:not(.ui) {
     font-weight: 700;
-    text-align: left;
     font-size: 1em;
     margin-top: -.21425em;
     line-height: 1.2em;
   }
-  .ui.cards>.card>.content>p {
-    font-size: 0.6em;  
-    text-align: left;
-    color: red;
-    background-color: #e8e8e8;
-    margin-bottom:1em;
+  .ui.cards>.card>.content {
+      text-align: left;
+    >.tags{
+        border-bottom: 1px solid #d7d7d7;
+        margin-bottom: .5em;
+        .ui.label{
+            font-size: 0.6em;  
+            margin-bottom:.5em;
+            margin-right:0.8em;
+            padding-left: 1em;
+            padding-right: 1em; 
+        }
+    } 
   }
   .ui.cards>.card>.image:not(.ui)>img {
     height: 150px;
@@ -147,17 +147,23 @@ export default {
   }
   .ui.cards>.card>.extra.content{
       padding: 0 0.5em 0 0;
+      border-top: none!important;
   }
-  .description>p{
-    font-size:0.6em;
-    text-align: left;
-    text-indent: 25px;  
+  .description{
+      padding:0 .5em;
+      
+      >p{
+        font-size:0.6em;
+        text-align: left;
+        text-indent: 25px;
+        border-bottom: 1px solid #d7d7d7;  
+      }
   }
   .ui.cards>.card>.content{
-    padding:1em 0 1em 0;  
+    padding:.5em;  
   }
   .ui.cards>.card{
-      min-height:350px;
+      min-height:300px;
   }  
 }
 
@@ -170,6 +176,13 @@ export default {
         .ui.cards>.card{
             min-height:300px;
         }
+        .ui.cards>.card>.image:not(.ui)>img {
+            height: 150px;
+        }
+        .ui.card>.content>.header:not(.ui), .ui.cards>.card>.content>.header:not(.ui) {
+            font-size: 1.5em;
+        }
+        
     }
 }
 </style>
