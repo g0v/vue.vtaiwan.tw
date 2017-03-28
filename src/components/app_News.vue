1c3ff6a3b449826a2899089e3d3b23dfe214c894
john5601 <john560148@gmail.com>
2017-03-28 11:56:57 +0800

    remove useless css

--- src/components/app_News.vue
+++ src/components/app_News.vue
@@ -130,63 +130,6 @@ export default {
       font-size: 50%;
     }
   }
-//   min-height: 72vh;
-//   .swiper-container1{
-    //   margin:4.5em 1em 0 3em;
-//   }
-  // .ui.card>.content>.header:not(.ui), .ui.cards>.card>.content>.header:not(.ui) {
-  //   font-weight: 700;
-  //   font-size: 1em;
-  //   margin-top: -.21425em;
-  //   line-height: 1.2em;
-  // }
-  // .ui.cards>.card>.content {
-  //     text-align: left;
-  //   >.tags{
-  //       // border-bottom: 1px solid #d7d7d7;
-  //       // margin-bottom: .5em;
-  //       .ui.label{
-  //           font-size: 0.6em;  
-  //           margin-bottom:.5em;
-  //           margin-right:0.8em;
-  //           padding-left: 1em;
-  //           padding-right: 1em; 
-  //       }
-  //   } 
-  // }
-  // .ui.cards>.card>.image {
-  //   max-height: 300px;
-  //   overflow: hidden;
-  //   img.logo{
-  //       display: block;
-  //       max-width: 200px;
-  //       height: auto;
-  //       margin:auto;
-  //   }
-  // }
-  // .ui.cards>.card [class*="right floated"] {
-  //   float: right;
-  //   font-size: 0.6em;
-  // }
-  // .ui.cards>.card>.extra.content{
-  //     padding: 0 0.5em 0 0;
-  //     border-top: none!important;
-  // }
-  // .description{
-  //     padding:0 .5em;
-      
-  //     >p{
-  //       font-size:0.6em;
-  //       text-align: left;
-  //       text-indent: 25px;
-  //       border-bottom: 1px solid #d7d7d7;  
-  //     }
-  // }
-  // .ui.cards>.card>.content{
-  //   padding:.5em;  
-  // }
-  // .ui.cards>.card{
-  //     min-height:300px;
-  // }  
+
 }
 </style>
