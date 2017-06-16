59d9cf1c358c75b90d852e7d347c5adaed95cc19
jokeum <joke0111@gmail.com>
2017-06-16 19:14:18 +0800

    fix observe fancybox would break in mobile view

--- src/components/home_observe.vue
+++ src/components/home_observe.vue
@@ -9,14 +9,12 @@
     |  Around the Globe
 
   .ui.container
-
-    #fancybox
+    .fancybox
       fancy(:locate="locate")
-
     .swiper-container3
       .swiper-pagination
       .swiper-wrapper
-        a.swiper-slide.ui.link.card(v-for="(n,idx) in allInfo", data-fancybox, data-src="#fancybox", href="#", @click.prevent ="myTitle = n.title")
+        a.swiper-slide.ui.link.card(v-for="(n,idx) in allInfo", data-fancybox, data-src=".fancybox", href="#", @click.prevent ="myTitle = n.title")
           .content
             .header
               h2 {{n.title}}
@@ -25,28 +23,9 @@
               span {{n.publish_date}}
             .description
               p.JQellipsis(v-html="n.content")
-
-    //- #mobile.swiper-container4.thin-only
-    //-   .swiper-pagination
-    //-   .swiper-wrapper
-    //-     a.swiper-slide.ui.link.card(v-for="(n,idx) in allInfo", data-fancybox='', data-src="#fancybox-2", href="javascript:;", v-on:click ="myTitle = n.title")
-    //-       .content
-    //-         .header
-    //-           h3 {{n.title}}
-    //-       .content
-    //-         .meta
-    //-           span {{n.publish_date}}
-    //-         .description
-    //-           p.JQellipsis(v-html="n.content")
-    //-       #fancybox-2(style='display: none;')
-    //-         fancy(:locate="locate")
-
 </template>
-
-
 <script>
 import fancy from "./home_observe_fancybox.vue"
-
 export default {
   name: 'observe',
   props:['allInfo'],
@@ -69,22 +48,6 @@ export default {
             }
         });
     },
-    // goAnchor: function(anchor){
-    //   if(anchor == "top"){
-    //     /* go to top */
-    //     $('html, body').animate({
-    //       scrollTop: 0,
-    //     }, 1000)
-    //   }
-    //   else if(anchor){
-    //     /* get the top position of anchor */
-    //     let anchor_y = $(anchor).offset().top
-    //     /* go to anchor (animation to do) */
-    //     $('html, body').animate({
-    //       scrollTop: anchor_y,
-    //     }, 1000)
-    //   }
-    // }
   },
   mounted: function () {
     this.ellipsis();
@@ -105,19 +68,6 @@ export default {
           }
         }
       })
-      // var mySwiper4 = new Swiper ('.swiper-container4', {
-      //   observer: true,
-      //   autoplay: 5000,
-      //   direction: 'horizontal',
-      //   pagination: '.swiper-pagination',
-      //   paginationType: 'fraction',
-      //   paginationClickable: true,
-      //   spaceBetween: 20,
-      // })
-      // mySwiper3.on('Init', this.ellipsis)
-      // mySwiper4.on('Init', this.ellipsis)
-
-      // $('#loader2').removeClass('active')
     })
   },
   updated:function() {
@@ -131,7 +81,6 @@ export default {
   }
 }
 </script>
-
 <style scoped lang="scss">
 @import "../sass/global.scss";
 .ui.container {
@@ -152,55 +101,17 @@ export default {
     }
   }
 }
-// #goTop{
-//     position: fixed;
-//     right: 30px;
-//     bottom: 30px;
-//     width: 40px;
-//     height:40px;
-//     padding: 10px 15px;
-//     font-size: 20px;
-//     border-radius: 50px;
-//     cursor: pointer;
-//     opacity: 0.8;
-//     z-index: 20;
-//     i.long.arrow.up.icon{
-//       width: auto;
-//     }
-// }
-#fancybox {
+.fancybox {
   display: none;
-  // width:70%;
-  // padding: 15px 40px 15px 32px;
-  // border-radius: 4px;
-  /* Custom transition - fade from top*/
   opacity: 0;
   transform: translateY(-50px);
   transition: all .5s;
-  // .ui.centered.card{
-  //   margin: auto;
-  //   width: auto;
-  //   height: auto;
-  // }
 }
-.fancybox-slide--complete #fancybox {
+.fancybox-slide--complete .fancybox {
   opacity: 1;
   transform: translateY(0);
 }
-// #fancybox-2 {
-
-//     width:98%;
-// 		padding: 15px 40px 15px 32px;
-// 		border-radius: 4px;
-
-//     /* Custom transition - fade from top*/
-// 		opacity: 0;
-//     transform: translateY(-50px);
-//     transition: all .5s;
-// 	}
-// 	.fancybox-slide--complete #fancybox-2 {
-// 		opacity: 1;
-// 		transform: translateY(0);
-// }
-
+.fancybox-slide > * {
+  padding: 24px 0;
+}
 </style>
